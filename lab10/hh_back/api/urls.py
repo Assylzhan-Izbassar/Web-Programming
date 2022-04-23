1259d80c20b7d1233a9dddfda21a5aa7e27ef113
Assylzhan Izbassar <macbook@Assylzhans-MacBook-Pro.local>
2022-04-23 11:43:04 +0600

    lab10

---
+++ lab10/hh_back/api/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from api.views import company_list, company_detail, company_vacancy, VacancyListAPIView, VacancyDetailAPIView, VacancyTopTenAPIView
+# from rest_framework_jwt.views import obtain_jwt_token
+
+urlpatterns = [
+    # path('login/', obtain_jwt_token),
+    path('companies/', company_list),
+    path('companies/<int:id>/', company_detail),
+    path('companies/<int:id>/vacancies/', company_vacancy),
+    path('vacancies/', VacancyListAPIView.as_view()),
+    path('vacancies/<int:id>/', VacancyDetailAPIView.as_view()),
+    path('vacancies/top_ten/', VacancyTopTenAPIView.as_view()),
+]
